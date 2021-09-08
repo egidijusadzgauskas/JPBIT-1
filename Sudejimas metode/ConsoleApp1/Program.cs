40d6164c829e79144f5774ee9c919ea7d7677bd7
raimondasruzgas <89936697+raimondasruzgas@users.noreply.github.com>
2021-09-08 19:23:21 +0300

    Kurimas
    Pridedu metoda sudejimui

---
+++ Sudejimas metode/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Skaicius = 9;
+            int Skaicius2 = 19;
+            ManoMetodas(Skaicius , Skaicius2 );
+                     
+        }
+        
+        static void ManoMetodas(int kintamasis, int kintamasis2)
+                
+        {
+            int skaiciuSuma = kintamasis + kintamasis2;
+            Console.WriteLine(skaiciuSuma);
+        }
+    }
+}
